0a93229278445b3197c263b86d628fa83f631241
David Valentine <davidlewisrogers3@gmail.com>
2019-11-09 13:10:38 -0800

    improve resize logic

--- display/cli.rb
+++ display/cli.rb
@@ -8,20 +8,27 @@ class Cli < Display
   attr_reader :width
   CLI_SPEED = 0.10
 
+  ### ISSUES (FIXME): 
+  # Resize: Going larger is fine, going smaller creates newlines
+  # Newlines: newlines are returned when typing.
+
   def initialize
     @width = term_width
-    Util.poll(2) { @width = term_width }
+    Util.poll(Cli::CLI_SPEED) { @width = term_width }
   end
 
   # TODO: accept multiple aggregators
   def stream(aggregator)
-    line = aggregator.feed.read ### HACK: this actually works a lot better than chunking. Might just need chunking for RSS feeds
+    ## Consider: This actually works just fine for disk files.
+    #            I think "chunking" would only be needed for
+    #            RSS feeds...
+    line = aggregator.feed.read
     @start = 0
-    @end = @width
+    @end = @start + @width
     loop do
       printf("\r#{line.slice(@start..@end)}")
       @start += 1
-      @end += 1 ### FIXME: doing it this way prevents the width adjust from working
+      @end = @start + @width
       sleep Cli::CLI_SPEED
       line += " * #{aggregator.feed.read}" if line[@end + 1].nil?
     end
