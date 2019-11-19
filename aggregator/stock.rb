917952f394af7f20f116b8b790323b8f4b369d69
David Valentine <davidlewisrogers3@gmail.com>
2019-11-18 18:40:08 -0800

    complete first version of stock

--- aggregator/stock.rb
+++ aggregator/stock.rb
@@ -7,7 +7,6 @@ require 'date'
 require_relative '../util.rb'
 
 ## Aggregator class for stock symbols, their current value and movement
-## TODO: Globalize this -- only US stocks work right now as far as I can tell.
 class Stock < Aggregator
   attr_reader :current_symbol
 
@@ -16,6 +15,7 @@ class Stock < Aggregator
   NO_CHANGE  = '(-)' # .yellow
   POS_CHANGE = '(▲)' # .green
   NEG_CHANGE = '(▼)' # .red
+  NO_VALUE   = '-'
   DAY_IN_SECONDS = 86_400
   CACHE_FILENAME = 'stock_cache.json'
 
@@ -33,7 +33,6 @@ class Stock < Aggregator
   ## Get the quote for the current symbol,
   #   move to the next one, and give the output string.
   def read
-    ## TODO: need to check cache validity here, too!!
     output_str = @cache[@current_symbol] || quote
     next_symbol
     output_str
@@ -68,7 +67,8 @@ class Stock < Aggregator
 
       cache_from_file = JSON.parse(File.read(Stock::CACHE_FILENAME))
 
-      mismatched_keys = cache_from_file.keys.length != @symbols.length # FIXME: Need to check if same length and a symbol changed
+      mismatched_keys = (cache_from_file.keys.length != @symbols.length) ||
+                        cache_from_file.keys != @symbols
       old_cache = !(file_ctime > yesterday && file_ctime < tomorrow)
 
       if old_cache || mismatched_keys
@@ -85,32 +85,38 @@ class Stock < Aggregator
 
   ## Hit the alphavantage API for the given symbol
   #   to get the current value and movement.
+  # Global stocks work, but the format is... different.
   def quote
     endpoint = 'https://www.alphavantage.co/'
     query = "query?function=GLOBAL_QUOTE&symbol=#{@current_symbol}&apikey=#{@api}"
 
-    begin
-      # Not using Kernel#Open, Rubocop is dumb
-      # rubocop:disable Security/Open
-      resp = JSON.parse(open("#{endpoint}#{query}").read)['Global Quote']
-      # rubocop:enable Security/Open
-    rescue StandardError ## TODO: determine other possible errors...
-      @value = 'N/A'
+    # Not using Kernel#Open, Rubocop is dumb
+    # rubocop:disable Security/Open
+    resp = begin
+             JSON.parse(open("#{endpoint}#{query}").read)['Global Quote']
+           rescue StandardError
+             nil
+           end
+    # rubocop:enable Security/Open
+
+    if resp.nil?
+      @value = Stock::NO_VALUE
       @movement = Stock::NO_CHANGE
+    else
+      @value = resp['05. price'].to_i
+      change = resp['09. change'].to_i
+
+      @movement = if change.positive?
+                    Stock::POS_CHANGE
+                  elsif change.negative?
+                    Stock::NEG_CHANGE
+                  elsif change.zero?
+                    Stock::NO_CHANGE
+                  else
+                    Stock::NO_CHANGE
+                  end
     end
 
-    @value = resp['05. price'].to_i
-    change = resp['09. change'].to_i
-
-    @movement = if change.positive?
-                  Stock::POS_CHANGE
-                elsif change.negative?
-                  Stock::NEG_CHANGE
-                elsif change.zero?
-                  Stock::NO_CHANGE
-                else
-                  Stock::NO_CHANGE
-                end
     @cache[@current_symbol] = "#{@current_symbol} $#{@value} #{@movement}"
   end
 
